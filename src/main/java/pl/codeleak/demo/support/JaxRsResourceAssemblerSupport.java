251c43aa98ff69fc7909207acc4b1d05b010dce3
kolorobot <rafal.borowiec@gmail.com>
2015-03-30 19:33:59 +0200

    Ooops. Added missing file... (#1)

---
+++ src/main/java/pl/codeleak/demo/support/JaxRsResourceAssemblerSupport.java
@@ -0,0 +1,33 @@
+package pl.codeleak.demo.support;
+
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.jaxrs.JaxRsLinkBuilder;
+import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
+import org.springframework.util.Assert;
+
+public abstract class JaxRsResourceAssemblerSupport<T, D extends ResourceSupport>
+        extends ResourceAssemblerSupport<T, D> {
+
+    private final Class<?> controllerClass;
+
+    public JaxRsResourceAssemblerSupport(
+            Class<?> controllerClass, Class<D> resourceType) {
+
+        super(controllerClass, resourceType);
+        this.controllerClass = controllerClass;
+    }
+
+    @Override
+    protected D createResourceWithId(Object id, T entity, Object... parameters) {
+        Assert.notNull(entity);
+        Assert.notNull(id);
+
+        D instance = instantiateResource(entity);
+
+        instance.add(
+                JaxRsLinkBuilder.linkTo(controllerClass, parameters)
+                        .slash(id)
+                        .withSelfRel());
+        return instance;
+    }
+}
